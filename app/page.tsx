48960f7d11267ef3c89aeb8e11662617a560f794
eyob13-coder <eyobgeremew618@gmail.com>
2025-06-04 02:20:20 +0300

    recent updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+"use client";
+
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
