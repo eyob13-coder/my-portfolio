b415f81f6cba64ec802d6e435a2294bc955f00c9
eyob13-coder <eyobgeremew618@gmail.com>
2025-06-04 12:29:42 +0300

    new

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
         <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
         <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple" />
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
-      </div>
+      
 
     
       <BackgroundBeamsWithCollision className="flex items-center justify-center">
@@ -52,6 +52,7 @@ const Hero = () => {
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black" />
       </div>
     </div>
+  </div>
   );
 };
 
